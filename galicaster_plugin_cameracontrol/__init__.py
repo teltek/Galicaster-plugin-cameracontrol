5d9f13040e8f63f2b7f940acf0c1ecbdb9eabd86
Daniel <daniel.peiteado@teltek.es>
2018-01-11 12:17:54 +0100

    Fix Galicaster version check

--- galicaster_plugin_cameracontrol/__init__.py
+++ galicaster_plugin_cameracontrol/__init__.py
@@ -9,7 +9,7 @@ except:
     print "Error: Galicaster not found"
 
 def init():
-    if LooseVersion(galicaster.__version__) <= LooseVersion("2.0.x"):
+    if LooseVersion(galicaster.__version__) >= LooseVersion("2.0.0"):
         cameracontrol.init()
     else:
         raise Exception("Plugin version mismatch")
