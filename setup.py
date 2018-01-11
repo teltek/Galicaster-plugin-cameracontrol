641ab479954a9a6356932bd765610ee43b69af7d
Daniel <daniel.peiteado@teltek.es>
2018-01-11 12:18:33 +0100

    Added requests dependence

--- setup.py
+++ setup.py
@@ -40,5 +40,5 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyserial'],
+    install_requires=['pyserial', 'requests'],
 )
